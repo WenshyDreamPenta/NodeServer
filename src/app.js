c3d59dc4ec7e2fdc8d01aacff4cffd04d8727cf7
Wangmingxing <starpentadream@gmail.com>
2018-06-13 16:30:34 +0800

    修改Json解析方式

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
